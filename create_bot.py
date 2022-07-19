43b9a47f7e1b3313f65e149ef10dfe63e8c19e71
KaLiNkA-code <76444754+KaLiNkA-code@users.noreply.github.com>
2022-07-19 20:11:03 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 """ async: Если в исполнении какой либо функции получился участок времени, когда ничего не используется,
 он позволяет в эти промежутки исполнять что то другое"""
 storage = MemoryStorage()
-token = '5460098484:AAH2GnJwqjzE6dAQAUNtmo2OCNPOwvdMo_U'
+token = 'SECRET'
 
 bot = Bot(token)
 dp = Dispatcher(bot, storage=storage)  # Инициализируем диспачер
